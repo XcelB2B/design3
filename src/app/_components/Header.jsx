201603052bf3c36ec5ab0464521b37ca8c4b722c
Riteshk_007 <codeshorts007@gmail.com>
2024-09-17 12:30:07 +0530

    Refactor package dependencies and add new components
    - Added "@radix-ui/react-slot" and "framer-motion" as new dependencies in package.json
    - Refactored import statements in layout.js and Header.jsx to fix component paths
    - Added new Button component in src/components/ui/button.jsx
    - Added new navbar menu components in

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
-const Header = () => {
-  return <div></div>;
-};
+import { HoveredLink, Menu, MenuItem } from "@/components/ui/navbar-menu";
+import { cn } from "@/lib/utils";
 
-export default Header;
+export function Header() {
+  return (
+    <div className="relative w-full flex items-center justify-center">
+      <Navbar className="top-2" />
+    </div>
+  );
+}
+
+function Navbar({ className }) {
+  const [active, setActive] = useState(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <HoveredLink href="/">Home</HoveredLink>
+
+        <MenuItem setActive={setActive} active={active} item="Services">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/web-development">
+              Web Development Services
+            </HoveredLink>
+            <HoveredLink href="/ui-ux-design">
+              UI/UX Design Services
+            </HoveredLink>
+            <HoveredLink href="/seo">
+              Search Engine Optimization Services
+            </HoveredLink>
+            <HoveredLink href="/branding">Branding Services</HoveredLink>
+            <HoveredLink href="/digital-marketing">
+              Digital Marketing Services
+            </HoveredLink>
+          </div>
+        </MenuItem>
+
+        <MenuItem setActive={setActive} active={active} item="About">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/about">Learn About Us</HoveredLink>
+            <HoveredLink href="/faq">Frequently Asked Questions</HoveredLink>
+          </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Contact">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/contact">Contact Us</HoveredLink>
+            <HoveredLink href="/careers">Career</HoveredLink>
+          </div>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
--- src/app/_components/HeroSection.jsx
+++ src/app/_components/HeroSection.jsx
@@ -1,7 +1,116 @@
 import React from "react";
 
 const HeroSection = () => {
-  return <div></div>;
+  return (
+    <div className="bg-black">
+      {" "}
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe dolores
+      non necessitatibus quisquam deleniti, doloremque dolorem minima voluptatum
+      quidem, fuga exercitationem quo corrupti repellat dolor quibusdam vel aut!
+      Libero optio natus, officia id odit nesciunt quibusdam illum, rerum
+      accusantium asperiores fuga. Voluptate veritatis sapiente incidunt non
+      laboriosam corrupti ea quo culpa nostrum laudantium ab, quidem numquam
+      voluptatibus impedit velit suscipit nisi autem necessitatibus tempora
+      repellendus. Sapiente doloremque explicabo, at repellat error molestiae
+      minima impedit obcaecati non cupiditate animi velit doloribus in libero
+      sunt quis, perspiciatis qui vero corporis ab voluptas aut sed. Cumque esse
+      provident repellat molestiae nesciunt nostrum incidunt harum inventore
+      laborum ullam itaque doloribus vel quaerat fugit unde fugiat praesentium,
+      veritatis, nisi quod ipsum iste? Voluptates et sit quaerat aspernatur
+      reiciendis saepe autem accusantium fugit pariatur delectus nulla culpa
+      laudantium illum ducimus eligendi nihil iste dolores, ipsum eos nisi
+      obcaecati. Illo error eius obcaecati odio ad repellendus iure alias non
+      perspiciatis, quod sit aliquam animi recusandae rerum facilis ut, cumque
+      eum illum et quae necessitatibus! Nulla in magnam dolore molestias fuga
+      doloremque ex, tempore soluta consequuntur autem! Rem ipsum eius
+      voluptatibus dolorem tenetur quos voluptas praesentium in, aperiam enim
+      deserunt a assumenda recusandae veniam, ex et molestiae quasi possimus
+      illum magni similique modi cupiditate neque! Quis in labore placeat, quos
+      amet odio possimus reprehenderit veritatis itaque eligendi asperiores
+      similique sed temporibus ipsum sunt saepe id adipisci eveniet. Ab odio
+      sint soluta itaque numquam sed iure, totam, ut enim rerum ex harum, saepe
+      magnam suscipit. Quia natus odit, voluptatem qui vitae velit numquam
+      consectetur, sunt delectus cumque laborum doloremque fuga beatae dolorem
+      quisquam nostrum, hic ipsa nulla porro nobis? Doloribus dolore ipsam quos
+      earum sit? Perspiciatis molestiae exercitationem omnis, porro asperiores
+      natus laborum excepturi dolorum voluptas nihil deserunt eveniet quo,
+      maiores mollitia error, ad deleniti nobis beatae. Sunt, vitae. Magni,
+      nisi, aspernatur nobis asperiores cumque ab in cum voluptatibus voluptates
+      ratione facilis atque laudantium nulla earum libero nostrum quae, iure
+      saepe optio pariatur. Eveniet est alias aut nisi, voluptas perferendis vel
+      iure recusandae iste exercitationem obcaecati. Eos ducimus officia,
+      adipisci accusamus placeat sequi ipsa ipsam, praesentium hic, ab totam id
+      saepe voluptatibus pariatur doloribus vitae. Consectetur repellat dolores
+      incidunt nemo vero quos eius. Quidem doloribus minus natus harum!
+      Perspiciatis unde nam rerum voluptatem. Similique culpa deserunt quae.
+      Quod porro, suscipit optio alias culpa eveniet. Sit voluptatum soluta
+      dignissimos architecto magni consequatur ratione voluptatibus in minima
+      totam distinctio, odio vero dolorem voluptate ad earum repudiandae iusto
+      est magnam, numquam veritatis. Iusto, illo nesciunt. Non nihil ea
+      consequuntur in, voluptatum explicabo placeat enim, quisquam molestiae
+      labore maiores repellat. In perspiciatis dignissimos assumenda ea. Labore
+      animi cum esse culpa optio delectus quaerat maxime ut totam ipsa
+      distinctio, quod facilis dolorum rerum qui laboriosam ad tempore officia.
+      Accusamus sint excepturi nemo perferendis ducimus eos pariatur dicta
+      facilis veniam. Accusamus hic aliquam, labore quis nihil quas eum soluta
+      tempora. Incidunt nostrum illum atque, non quaerat amet fugit quo ea sint
+      fugiat eum, aut mollitia dolorum ad! Magni quia deserunt, eius obcaecati
+      aliquam nisi error dolor saepe quo quod soluta deleniti doloremque tenetur
+      veritatis consequuntur ducimus labore dignissimos magnam, et velit dicta.
+      Sequi sint impedit quisquam nobis itaque eaque minima nemo, labore
+      reiciendis voluptates ducimus ullam officiis fugiat aliquam aperiam
+      necessitatibus. Fugit expedita soluta pariatur, animi voluptatum, odit
+      consectetur quas excepturi iste molestiae suscipit asperiores reiciendis
+      deleniti! Molestias doloribus reprehenderit iusto similique, modi iure
+      saepe dicta maxime at aut dolores dolore odio natus, nesciunt magnam
+      voluptas odit ratione consectetur dignissimos veritatis dolorem inventore
+      doloremque! Ut debitis nesciunt ipsum perspiciatis modi dolorem velit,
+      voluptate quia odit tenetur vero minima necessitatibus saepe obcaecati
+      laboriosam aliquam sint enim pariatur harum excepturi eveniet assumenda
+      aliquid. Sed, enim debitis unde dolorem nihil magni consequatur molestiae
+      perferendis modi optio, maiores natus ipsa autem hic! Dolorum, pariatur!
+      Cumque reprehenderit ducimus consequatur, ab nostrum omnis labore tenetur,
+      natus praesentium vel voluptatibus odio fugiat, beatae excepturi? Impedit
+      sit qui provident recusandae nam consectetur nesciunt nisi, minima
+      molestiae voluptatem, reiciendis fuga illum consequuntur possimus minus
+      fugiat quia quidem, quod obcaecati iusto. Praesentium consequuntur
+      voluptas expedita neque eos quos, ab hic odit dicta velit quas mollitia
+      voluptatibus quis accusantium id minus sunt possimus eius tempore tenetur
+      omnis fuga! Temporibus vero facere, minus ipsum voluptas rem laudantium
+      fuga vitae, assumenda exercitationem molestias dolorem eaque aut maiores
+      molestiae hic. Animi non nostrum libero nobis. Doloribus, velit! Corporis,
+      deleniti sapiente. Ad unde quae amet enim, repellat velit. Tempore
+      aspernatur architecto praesentium quas amet quidem ipsam et soluta rem
+      unde, sapiente maiores nulla exercitationem, magni itaque harum quod ex
+      officia nihil repellendus cum saepe earum. Molestias placeat
+      exercitationem accusantium quisquam sapiente totam, cumque labore, porro
+      necessitatibus amet illum ducimus temporibus, quis eligendi vero. Ipsum,
+      officiis alias? Quibusdam quisquam quos provident omnis exercitationem
+      unde animi qui dolor cumque quasi sequi possimus dolore ducimus voluptatem
+      harum vero facere blanditiis, corrupti perferendis odit ab! Dolores,
+      consectetur! Tempora atque aliquam eligendi excepturi id necessitatibus a
+      sint veritatis sequi tempore est modi nemo, harum assumenda fugiat beatae
+      dolorum similique, illum vero eum. Iure, explicabo neque aliquid ea
+      deserunt veniam sed nihil a, cum dolorem vero modi laudantium, totam
+      adipisci? Repellendus odio repellat quas fuga aspernatur quidem harum,
+      blanditiis labore velit, incidunt officia accusantium placeat, nam saepe!
+      Id sint fuga similique dolorem dolore omnis voluptatem minima tempore
+      perspiciatis velit corporis aperiam, expedita earum perferendis, dolorum
+      eligendi dolores atque, quis aliquid rerum nulla distinctio modi. Nostrum,
+      aliquid. Pariatur sed aut reiciendis dolorem ex, officia molestias nam
+      omnis non ducimus quos a distinctio ea repellendus eos? Sunt cum atque
+      odio? Qui odio deleniti esse optio quidem commodi incidunt non tenetur
+      reiciendis ad nam eius eaque perferendis quae, aliquam ipsum aspernatur.
+      Doloribus eligendi inventore molestias quo fugit aut explicabo soluta hic
+      voluptatem. Laborum enim aut veniam dicta, sint illum culpa dignissimos
+      voluptate esse magnam id voluptates tempore repellat dolorem facilis
+      eveniet optio a. Excepturi est, atque dolorum nisi aliquid odit error
+      asperiores obcaecati aspernatur omnis porro labore magnam incidunt ullam
+      cum reprehenderit culpa optio similique repellat officiis, sint iure
+      doloremque. Perspiciatis quae delectus cumque aut doloribus quis,
+      veritatis velit!
+    </div>
+  );
 };
 
 export default HeroSection;
